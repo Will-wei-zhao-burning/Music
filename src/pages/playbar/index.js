dd5b5ca81935a25bf665b6e5cd6a4e698eb3f685
Will <775817124@qq.com>
2021-03-05 17:12:09 +0800

    底部playbar的样式

--- src/pages/playbar/index.js
+++ src/pages/playbar/index.js
@@ -4,10 +4,12 @@
  * @Author: BurNing
  * @Date: 2021-03-03 16:22:24
  * @LastEditors: BurNing
- * @LastEditTime: 2021-03-03 16:57:20
+ * @LastEditTime: 2021-03-05 11:40:37
  */
 import React, { memo } from 'react'
+import { NavLink } from 'react-router-dom'
 
+import { Slider } from 'antd';
 import {PlayBarWrapper,Control,Info,Operation} from './style'
 
 export default memo(function PlayBar() {
@@ -15,13 +17,49 @@ export default memo(function PlayBar() {
         <PlayBarWrapper>
            <div className="wrap-v2">
                <Control>
-                   <button className="prev"></button>
-                   <button className="play"></button>
-                   <button className="next"></button>
+                   <button onClick={e=>{prev()}} className="sprite_player prev "></button>
+                   <button className="sprite_player play"></button>
+                   <button className="sprite_player next"></button>
                </Control>
-               <Info></Info>
-               <Operation></Operation>
+               <Info>
+                   <div className="song-pic">
+                       <NavLink to="/discover">
+                           <img src="https://p2.music.126.net/0fL8vcI942Wocdt4M1dp4A==/109951165547470592.jpg?param=34y34"></img>
+                       </NavLink>
+                   </div>
+                   <div className="song-detail">
+                       <div className="title">
+                           <span className="song-name">雪花</span>
+                           <span className="singer-name">
+                               <a>叙述</a>
+                           </span>
+                       </div>
+                       <div className="slider">
+                           <Slider defaultValue={30} ></Slider>
+                           <div className="timer">
+                               <span className="now-time">02:11</span>
+                               <span className="divider">/</span>
+                               <span>04:30</span>
+                           </div>
+                       </div>
+                   </div>
+               </Info>
+               <Operation>
+                   <div className="operation">
+                       <button className="sprite_player add"></button>
+                       <button className="sprite_player forward"></button>
+                   </div>
+                   <div className="sprite_player setting">
+                       <button className="sprite_player voice"></button>
+                       <button className="sprite_player cycle"></button>
+                       <button className="sprite_player list"></button>
+                   </div>
+               </Operation>
            </div>
         </PlayBarWrapper>
     )
+    
+    function prev(){
+        console.log("播放上一首歌曲；")
+    }
 })
--- src/pages/playbar/style.js
+++ src/pages/playbar/style.js
@@ -4,7 +4,7 @@
  * @Author: BurNing
  * @Date: 2021-03-03 16:22:30
  * @LastEditors: BurNing
- * @LastEditTime: 2021-03-03 17:00:41
+ * @LastEditTime: 2021-03-05 11:42:32
  */
 import styled from 'styled-components'
 
@@ -27,10 +27,132 @@ export const PlayBarWrapper = styled.div`
   } */
 `
 export const Control = styled.div`
-   
+   display:inline-block;
+   width: 137px;
+   padding: 6px 0 0 0;
+   button{
+    width: 28px;
+    height: 28px;
+    margin-right: 8px;
+    margin-top: 5px;
+   }
+   .prev{
+    background-position: 0 -130px;
+   }
+
+   .play{
+    width: 36px;
+    height: 36px;
+    margin-top: 0;
+    background-position: 0 -204px;
+   }
+
+   .next{
+    background-position: -80px -130px;
+   }
 `
 
-export const Info = styled.div``
+export const Info = styled.div`
+   display:flex;
+   align-items: center;
+   justify-content:space-between;
+   flex:1;
 
-export const Operation = styled.div``
+   .song-pic{
+       width:34px;
+       height:34px;
+       margin:6px 15px 0 0;
+   }
+   .song-detail{
+       width:605px;
+       color: #a1a1a1;
+
+       .title{
+        margin-left:7px;
+        height: 28px;
+        line-height:28px;
+         .song-name{
+             margin-right:10px;
+         }
+       }
+       .slider{
+        display: flex;
+      align-items: center;
+      .ant-slider {
+        width: 493px;
+        margin-right: 10px;
+        margin-top:0px;
+        margin-bottom:0px;
+
+        .ant-slider-rail {
+          height: 9px;
+          background: url(${require("@/assets/img/progress_bar.png").default}) right 0;
+        }
+
+        .ant-slider-track {
+          height: 9px;
+          background: url(${require("@/assets/img/progress_bar.png").default}) left -66px;
+        }
+
+        .ant-slider-handle {
+          width: 22px;
+          height: 24px;
+          border: none;
+          margin-top: -7px;
+          background: url(${require("@/assets/img/sprite_icon.png").default}) 0 -250px;
+          }
+        }
+        .timer {
+        .now-time {
+          color: #e1e1e1;
+        }
+        .divider {
+          margin: 0 3px;
+        }
+      }
+        
+       }
+   }
+`
+
+export const Operation = styled.div`
+   display:inline-block;
+   position:relative;
+
+   button{
+    width: 25px;
+    height: 25px;
+    margin: 11px 2px 0 0;
+   }
+   .operation{
+       width:60px;
+       display:inline-block;
+       .add{
+        background-position: -88px -163px;
+       }
+       .forward{
+        background-position: -114px -163px;
+       }
+   }
+   .setting{
+       display:inline-block;
+       width: 128px;
+       padding-left:13px;
+       background-position: -147px -238px;
+
+       .voice{
+        background-position: -2px -248px;
+
+       }
+       .cycle{
+        background-position: -3px -344px;
+       }
+       .list{
+        width: 59px;
+    height: 25px;
+    background-position: -42px -68px;
+    line-height: 27px;
+       }
+   }
+`
 
