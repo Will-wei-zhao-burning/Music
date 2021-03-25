b42e7ba723c43028a18ce3115b88532750e0f58f
Will <775817124@qq.com>
2021-03-25 20:53:16 +0800

    添加getLyric请求

--- src/service/player.js
+++ src/service/player.js
@@ -4,7 +4,7 @@
  * @Author: BurNing
  * @Date: 2021-03-05 17:22:52
  * @LastEditors: BurNing
- * @LastEditTime: 2021-03-05 17:48:11
+ * @LastEditTime: 2021-03-25 20:52:47
  */
 import request from './request'
 
@@ -14,5 +14,13 @@ export function getSongDetail(ids){
       params:{
           ids
       }
+  })
+}
+export function getLyric(id){
+  return request({
+    url:'/lyric',
+    params:{
+      id
+    }
   })
 }
